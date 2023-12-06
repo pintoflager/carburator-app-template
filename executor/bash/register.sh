e9ff6c038b0e9580bbfbe792fa755e25cbfdd7c1
Jaakko Eronen <jaakko0606@gmail.com>
2023-12-06 14:20:43 -0500

    destroy resources try vol 1

--- executor/bash/register.sh
+++ executor/bash/register.sh
@@ -5,11 +5,11 @@ role="$1"
 
 # App installation tasks on a client node. Runs first
 if [ "$role" = 'client' ]; then
-    carburator print terminal info "Executing register script on $role"
+    carburator log info "Executing register script on $role"
 
 # App installation tasks on remote server node.
 elif [ "$role" = 'server' ]; then
-    carburator print terminal info "Executing register script on $role"
+    carburator log info "Executing register script on $role"
 
 else
     echo "Playing with CLI: $role"
